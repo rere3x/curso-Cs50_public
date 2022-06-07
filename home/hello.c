cda65fb9e28bb14c9b5d00aaa985c1c2dc81876f
rere3x <104224827+rere3x@users.noreply.github.com>
2022-06-07 23:34:31 +0000

    Tue, Jun 7, 2022, 11:34 PM +00:00

--- home/hello.c
+++ home/hello.c
@@ -11,11 +11,11 @@ int main(void)
     int i = positive_int();
 
 
-    alturaDosBlocos(i);
+    //alturaDosBlocos(i);
 
 }
 
-void alturaDosBlocos(int n)
+/*void alturaDosBlocos(int n)
 {
     int largura = n;
 
@@ -32,15 +32,15 @@ void alturaDosBlocos(int n)
             printf("#");
         }
     }
-
 }
+*/
 
 int positive_int(void)
 {
     int n;
     do
     {
-        n = get_int("Escolha um numero positivo: "\n);
+        n = get_int("Escolha um numero positivo: \n");
     }
     while (n < 1);
     return n;
