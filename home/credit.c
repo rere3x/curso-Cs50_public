a683a29e1673ef1d6e776736bb77d2c90be4cf74
rere3x <104224827+rere3x@users.noreply.github.com>
2022-06-10 21:48:19 +0000

    Fri, Jun 10, 2022, 9:48 PM +00:00

--- home/credit.c
+++ home/credit.c
@@ -4,12 +4,19 @@
 int main(void)
 {
 
-   long cartao = get_long("Teste seu cartão de credito: \n");
-
+    long cartao = get_long("Teste seu cartão de credito: \n");
+    int numerchecked = numbercheck(cartao);
 
    //remender test
 
     long remender = cartao % 10000000000000000;
     printf("remender = %li\n", remender);
+
+}
+
+int numbercheck(long cartao)
+{
+    for ()
+
 
 }
