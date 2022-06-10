cd8b1db03f54c2e1e8122eaa35b13db3c40d4687
rere3x <104224827+rere3x@users.noreply.github.com>
2022-06-10 17:21:11 +0000

    Fri, Jun 10, 2022, 5:21 PM +00:00

--- home/coins.c
+++ home/coins.c
@@ -10,16 +10,38 @@ int main(void)
     float p = get_float("Qual valor do item? ");
     float i = get_money();
     float t = troco (p,i);
-    float 
-
-
-}
-
 
     int coin25;
     int coin10;
     int coin05;
     int coin01;
+
+ if(t > 0.25)
+    {
+        resultado /= 0.25;
+
+
+    }
+    if(t > 0.10)
+    {
+        resultado /= 0.10;
+
+    }
+    if(t > 0.05)
+    {
+        resultado /= 0.05;
+
+    }
+    if(t > 0.01)
+    {
+        resultado /= 0.01;
+
+    }
+
+
+
+}
+
 
 
 
