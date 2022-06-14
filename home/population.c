e5961c1abcf3065cc4f2675ac12872e2e059fcef
rere3x <104224827+rere3x@users.noreply.github.com>
2022-06-14 00:47:23 +0000

    Tue, Jun 14, 2022, 12:47 AM +00:00

--- home/population.c
+++ home/population.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 #include<cs50.h>
 
-
+int yearsToLimit(population,endSize);
 int positive_int(string s);
+
+
+int population;
+int endSize;
 
 int main(void)
 {
@@ -11,9 +15,9 @@ int main(void)
     Years: 9
     */
 
-    int n = positive_int("Qual tamanho inicial da população: \n");
-    int b = positive_int("Qual tamanho final da população: \n");
-    yearsToLimit(n,n);
+    population = positive_int("Qual tamanho inicial da população: \n");
+    endSize = positive_int("Qual tamanho final da população: \n");
+    int yearsToLimit(population,endSize);
 
 }
 
@@ -32,7 +36,16 @@ int positive_int(string s)
     return n;
 }
 
-yearsToLimit(int n, int b)
+int yearsToLimit(int n, int b)
 {
-    
+    int years = 0;
+    do
+    {
+
+    int newPopulation = population + n - b;
+    years++;
+    }
+    while (newPpulation != endSize)
+
+    return years;
 }
