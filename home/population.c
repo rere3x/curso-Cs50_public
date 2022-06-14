fa75c3ed91aa2e234451c4bf95e0d02e4a486914
rere3x <104224827+rere3x@users.noreply.github.com>
2022-06-14 01:24:50 +0000

    Tue, Jun 14, 2022, 1:24 AM +00:00

--- home/population.c
+++ home/population.c
@@ -19,7 +19,7 @@ int main(void)
     population = positive_int("Qual tamanho inicial da população: \n");
     endSize = positive_int("Qual tamanho final da população: \n");
     int years = yearsToLimit(population , endSize);
-    printf("%i\n", years);
+    //printf("%i\n", years);
 }
 
 
@@ -49,7 +49,7 @@ int yearsToLimit(int n, int b)
     while (newPopulation >= endSize)
     {
         newPopulation = n + (n/3) - (n/4) ;
-    printf
+    printf("%i\n", newPopulation);
         years++;
     }
 
