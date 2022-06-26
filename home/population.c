9f43ce90bea70da41fc7a1f013b35675ae17e72f
rere3x <104224827+rere3x@users.noreply.github.com>
2022-06-26 21:11:15 +0000

    Sun, Jun 26, 2022, 9:11 PM +00:00

--- home/population.c
+++ home/population.c
@@ -43,8 +43,10 @@ int yearsToLimit(int n, int b)
 {
     if (n == b)
     return = 0;
-    else
-    newpop = n + (n / 3) + (n / 4)
+
+    newpop = n + (n / 3) + (n / 4);
+    
+
 
     return years;
 
