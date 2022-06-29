de0dd1b2caa2177c1af2a047499f43dd11010ef9
rere3x <104224827+rere3x@users.noreply.github.com>
2022-06-29 18:47:36 +0000

    Wed, Jun 29, 2022, 6:47 PM +00:00

--- home/readability.c
+++ home/readability.c
@@ -56,7 +56,7 @@ int textFormula(string text)
             letters++;
 
         //checking for non-lettlers like ". , ? :"
-        if(isalnum(t[i]) )
+        if(!isalnum(t[i]))
             sentenses++;
     }
 
