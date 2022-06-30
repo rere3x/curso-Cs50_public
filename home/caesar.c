2fe0535a7771402105c32f5b11430f80dd353ad9
rere3x <104224827+rere3x@users.noreply.github.com>
2022-06-30 16:19:51 +0000

    Thu, Jun 30, 2022, 4:19 PM +00:00

--- home/caesar.c
+++ home/caesar.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <ctype.h>
 
 
 int main(int arg, string argv[])
 {
-    if (isdigit(argv[1])  && argv[2] == \0 )
 
-        
+    if ( validKey() && argv[2] == \0 )
+
+
 
 
     else
     printf("Usage: ./caesar key\n");
 
 
+}
+
+int validKey (void)
+{
+    for (int i = 0, n = strlen(argv[1]); i < n; i++)
+
+        if
 }
