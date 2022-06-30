4971ddae2809762ef1f50d684e1536861aac63b5
rere3x <104224827+rere3x@users.noreply.github.com>
2022-06-30 23:24:56 +0000

    Thu, Jun 30, 2022, 11:24 PM +00:00

--- home/caesar.c
+++ home/caesar.c
@@ -13,7 +13,7 @@ int main(int arg, string argv[])
 
     if (validKey(strlen( argv[1]), argv) == 1 && argv[1])
     {
-            
+
         string text = get_string("Text: \n");
         ciperText(strlen(text), text, atoi(argv[1]));
 
@@ -55,8 +55,8 @@ void ciperText(int length, string s, int ciper)
         for (int i = 0; i < length; i++)
 {
 
-            printf("%i ", s[i]);
-            printf("%i\n",  ciper);
+
+            
 
             if (isupper(s[i]) && isalpha(s[i]))
                 printf("%c", ((s[i] + ciper) % 26) + 65);
