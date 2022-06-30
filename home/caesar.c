13dca0bf3467587cea42e6ff1d21da490d51e03b
rere3x <104224827+rere3x@users.noreply.github.com>
2022-06-30 16:12:59 +0000

    Thu, Jun 30, 2022, 4:12 PM +00:00

---
+++ home/caesar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+
+int main(int arg, string argv[])
+{
+    if (isdigit(argv[1])  && argv[2] == \0 )
+
+        
+
+
+    else
+    printf("Usage: ./caesar key\n");
+
+
+}
