1fa2a0a0305deeeebcc814695d404959fd97fc61
rere3x <104224827+rere3x@users.noreply.github.com>
2022-07-01 11:40:03 +0000

    Fri, Jul 1, 2022, 11:40 AM +00:00

--- home/scrabble.c
+++ home/scrabble.c
@@ -20,11 +20,11 @@ int main(void)
 
     // TODO: Print the winner
     if (score1 < score2)
-        printf("Player 1 wins!");
+        printf("Player 1 wins!\n");
     else if(score1 == score2)
-        printf("Tie!");
+        printf("Tie!\n");
     else
-        printf("Player 2 wins!");
+        printf("Player 2 wins!\n");
 }
 
 int compute_score(string word)
