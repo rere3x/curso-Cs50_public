04d608919ae3827289ab8cc344d812234de4a91d
rere3x <104224827+rere3x@users.noreply.github.com>
2022-07-04 15:35:49 +0000

    Mon, Jul 4, 2022, 3:35 PM +00:00

--- home/plurality.c
+++ home/plurality.c
@@ -66,8 +66,8 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-        for (int i = 0; i < canditade_count; i+)
-            if( strcmp( candidates[i], name) == 0)
+        for (int i = 0; i < candidate_count; i++)
+            if( strcmp( candidates[i].name, name) == 0)
     return false;
 }
 
