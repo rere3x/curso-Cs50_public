dd1e155f0575b796d72308431efb671be45797ff
rere3x <104224827+rere3x@users.noreply.github.com>
2022-08-21 00:13:25 +0000

    ccc, August 21, 2022 12:13 AM 0, 2424, 12:13 am +0000

--- pset3/plurality/plurality.c
+++ pset3/plurality/plurality.c
@@ -87,15 +87,17 @@ void print_winner(void)
 */
     int mairoVoto = 0;
 
-    for (int i = 0; i < candidate_count - 1; i++)
+    for (int i = 0; i < candidate_count -1; i++)
     {
         if (candidates[i].votes > candidates[i + 1].votes)
         maiorVoto = candidates[i].votes;
     }
+
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes == candidates[i + 1].votes)
+        if (maiorVoto == candidates[i].votes)
         
+
     }
 
 
