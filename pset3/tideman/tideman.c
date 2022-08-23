280bb38010349d028f843c54154daeea63a52500
rere3x <104224827+rere3x@users.noreply.github.com>
2022-08-23 20:35:24 +0000

    Tue, Aug 23, 2022, 8:35 PM +00:00

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -104,7 +104,7 @@ bool vote(int rank, string name, int ranks[])
     {
         if (strcmp (name, candidates[i].name) == 0)
         {
-            ranks[i] = true;
+            preferences[ranks][] = true;
 
             return true;
         }
