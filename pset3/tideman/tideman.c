1041caa3a3cd71e2c9b46e71bb6e09ef01cb1e8d
rere3x <104224827+rere3x@users.noreply.github.com>
2022-08-25 09:14:24 +0000

    Thu, Aug 25, 2022, 9:14 AM +00:00

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -117,7 +117,7 @@ bool vote(int rank, string name, int ranks[])
 void record_preferences(int ranks[])
 {
     // TODO
-        for (int i = 0; i < candidate_count; i++)
+        for (int i = 0; i < candidate_count - 1; i++)
         {
 
             for (int j = 0; j < candidate_count; j++)
