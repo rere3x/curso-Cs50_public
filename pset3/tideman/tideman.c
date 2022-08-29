e9f731f492cd6b60f8831ab1b6638657554cfa24
rere3x <104224827+rere3x@users.noreply.github.com>
2022-08-29 23:30:43 +0000

    Mon, Aug 29, 2022, 11:30 PM +00:00

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -136,6 +136,9 @@ void record_preferences(int ranks[])
 void add_pairs(void)
 {
     // TODO
+
+preference [pessoa] [gostam menos]
+
     for (int i = 0; i < candidate_count; i++)
         {
             for (int j = 0; j < candidate_count; j++)
