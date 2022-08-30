2f2a79ee59c593c4483f2b4894459d8e5c912511
rere3x <104224827+rere3x@users.noreply.github.com>
2022-08-30 02:24:43 +0000

    Tue, Aug 30, 2022, 2:24 AM +00:00

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -148,15 +148,6 @@ void add_pairs(void)
                     pairs[pair_count].loser = j;
                     pair_count++;
                 }
-                else if(preferences[i][j] == preferences[j][i])
-                {
-                    pairs[pair_count].winner = i;
-                    pairs[pair_count + 1].winner = j;
-                    pairs[pair_count].loser = i;
-                    pairs[pair_count + 1].loser = j;
-                    pair_count++;
-                    pair_count++;
-                }
 
                 else
                 {
