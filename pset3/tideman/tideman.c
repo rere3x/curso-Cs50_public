961b694baf6fbe7cc7d670d2dd7cd1cca505e000
rere3x <104224827+rere3x@users.noreply.github.com>
2022-09-07 00:52:50 +0000

    Wed, Sep 7, 2022, 12:52 AM +00:00

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -174,7 +174,7 @@ void sort_pairs(void)
     int sortedPairs [pair_count][pair_count];
         for(int i = 0; i < pair_count; i++)
             {
-                sortedPairs [pairs[pair_count].winner][pairs[pair_count].loser] = preferences[pairs[pair_count].winner] - preferences[pairs[pair_count].loser];
+                sortedPairs [pairs[i].winner][pairs[i].loser] = preferences[pairs[i].winner] - preferences[pairs[i].loser];
 
             }
 
