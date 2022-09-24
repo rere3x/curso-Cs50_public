0ba791ed7aba9e7438bb44f7617c21e6ff6f7657
rere3x <104224827+rere3x@users.noreply.github.com>
2022-09-24 00:47:44 +0000

    Sat, Sep 24, 2022, 12:47 AM +00:00

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -170,34 +170,30 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    // TODO
+    int min_idx;
+    int max_strength;
 
-        int indx;
-        int greatNum;
+    for (int i = 0; i < pair_count - 1; i++)
+    {
+        min_idx = i;
+        max_strength = preferences[pairs[i].winner][pairs[i].loser];
 
-    for(int i = 0; i < pair_count; i++)
+        for (int j = i + 1; j < pair_count; j++)
         {
-            indx = i;
-            greatNum = preferences[pairs[i].winner][pairs[i].loser];
-
-                for(int j = i + 1; j < pair_count; j++)
-                    {
-                        if(preferences[pairs[j].winner][pairs[j].loser] > greatNum)
-
-                        greatNum = preferences[pairs[j].winner][pairs[j].loser];
-                        indx = j;
-                    }
-            if(indx != i)
+            if (preferences[pairs[j].winner][pairs[j].loser] > max_strength)
             {
-            pair temp = pairs[indx];
-            pairs[indx] = pairs[i];
-            pairs[i] = temp;
+                max_strength = preferences[pairs[j].winner][pairs[j].loser];
+                min_idx = j;
             }
-
         }
 
-
-
+        if (min_idx != i)
+            {
+                pair temp = pairs[min_idx];
+                pairs[min_idx] = pairs[i];
+                pairs[i] = temp;
+            }
+    }
     return;
 }
 
