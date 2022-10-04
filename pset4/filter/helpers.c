81e5a1b397fd8e5faa5b79dbddd94a44ad22b427
rere3x <104224827+rere3x@users.noreply.github.com>
2022-10-04 16:31:58 +0000

    Tue, Oct 4, 2022, 4:31 PM +00:00

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -59,7 +59,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j <= width; j++)
         {
-            RGBTRIPLE tempImage [i][j];
+            tempImage [i][j];
         }
     }
 
