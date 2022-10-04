17242ea33b5a2f3460d6d85be0c8be9c328962b6
rere3x <104224827+rere3x@users.noreply.github.com>
2022-10-04 15:06:40 +0000

    Tue, Oct 4, 2022, 3:06 PM +00:00

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -28,6 +28,8 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     int hafWidth = 0;
     if(width % 2 == 0)
         hafWidth = (width / 2) - 1;
+    else if (width == 1)
+        hafWidth =1;
     else
         hafWidth = (width / 2) - 0.5;
 
