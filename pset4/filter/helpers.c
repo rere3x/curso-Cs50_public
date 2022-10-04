ec33073077c3797e4c2f517a975dfefc4388bab0
rere3x <104224827+rere3x@users.noreply.github.com>
2022-10-04 13:03:16 +0000

    Tue, Oct 4, 2022, 1:03 PM +00:00

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -25,6 +25,14 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+     for (int i = 0; i <= height; i++)
+    {
+        for (int j = 0; j <= width; j++)
+        {
+            
+        }
+    }
+
     return;
 }
 
