e85fe1277c9440543d9df74e42f535177c3e2050
rere3x <104224827+rere3x@users.noreply.github.com>
2022-10-26 01:31:31 +0000

    Wed, Oct 26, 2022, 1:31 AM +00:00

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -164,7 +164,6 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                 }
                 colum++;
             }
-            /*
             image[i][j].rgbtRed = (int)round(sqrt( pow(somaGx[0],2) + pow(somaGy[0],2)));
             image[i][j].rgbtGreen = (int)round(sqrt( pow(somaGx[1],2) + pow(somaGy[1],2)));
             image[i][j].rgbtBlue = (int)round(sqrt( pow(somaGx[2],2) + pow(somaGy[2],2)));
@@ -172,8 +171,8 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             if(image[i][j].rgbtRed > 255) image[i][j].rgbtRed = 255;
             if(image[i][j].rgbtGreen > 255) image[i][j].rgbtGreen = 255;
             if(image[i][j].rgbtBlue > 255) image[i][j].rgbtBlue = 255;
-            */
 
+            /*
              if(sqrt( pow(somaGx[0],2) + pow(somaGy[0],2)) > 255.00)
                 image[i][j].rgbtRed = 255;
                 else
@@ -188,6 +187,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                 image[i][j].rgbtBlue = 255;
                 else
                 image[i][j].rgbtBlue = (int)round(sqrt( pow(somaGx[2],2) + pow(somaGy[2],2)));
+            */
         }
 
 /*
