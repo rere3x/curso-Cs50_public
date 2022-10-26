d463bf439227565f999118dbe29860c566e47a73
rere3x <104224827+rere3x@users.noreply.github.com>
2022-10-26 02:40:20 +0000

    Wed, Oct 26, 2022, 2:40 AM +00:00

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -192,13 +192,21 @@ for (int i = 0; i < height; i++)
                         gycolum++;
 
             }
+            if((sqrt( pow(Gx[0],2) + pow(Gy[0],2))) > 255) image[i][j].rgbtRed = 255;
+            else
             image[i][j].rgbtRed = (int)round(sqrt( pow(Gx[0],2) + pow(Gy[0],2)));
-            image[i][j].rgbtGreen = (int)round(sqrt( pow(Gx[1],2) + pow(Gy[1],2)));
-            image[i][j].rgbtBlue = (int)round(sqrt( pow(Gx[2],2) + pow(Gy[2],2)));
 
-            if(image[i][j].rgbtRed > 255) image[i][j].rgbtRed = 255;
-            if(image[i][j].rgbtGreen > 255) image[i][j].rgbtGreen = 255;
-            if(image[i][j].rgbtBlue > 255) image[i][j].rgbtBlue = 255;
+            if((sqrt( pow(Gx[1],2) + pow(Gy[1],2))) > 255) image[i][j].rgbtRed = 255;
+            else
+            image[i][j].rgbtRed = (int)round(sqrt( pow(Gx[1],2) + pow(Gy[1],2)));
+
+            if((sqrt( pow(Gx[2],2) + pow(Gy[2],2))) > 255) image[i][j].rgbtRed = 255;
+            else
+            image[i][j].rgbtRed = (int)round(sqrt( pow(Gx[2],2) + pow(Gy[2],2)));
+
+
+
+
 
             }
         }
