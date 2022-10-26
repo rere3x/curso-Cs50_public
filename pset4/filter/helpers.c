56c22f396a9931dfd9f74167b528e386d8fa96ab
rere3x <104224827+rere3x@users.noreply.github.com>
2022-10-26 00:37:24 +0000

    Wed, Oct 26, 2022, 12:37 AM +00:00

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -119,11 +119,13 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
     int Gx[3][3]  =  {{-1, 0, 1},
                       {-2, 0, 0},
                       {-1, 0, 1}};
-                    
+    float somaGx = 0;
 
     int Gy[3][3]  =  {{-1,-2,-1},
                       {0, 0, 0},
                       {1, 2, 1}};
+
+    float somaGy = 0;
 
     for (int i = 0; i < height; i++)
     {
