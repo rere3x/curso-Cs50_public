c54298bb6163750d5b4670dd170de679882e7a25
rere3x <104224827+rere3x@users.noreply.github.com>
2022-10-27 21:56:50 +0000

    Thu, Oct 27, 2022, 9:56 PM +00:00

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -3,7 +3,20 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    for (int i = 0; i <= height; i++)
+    {
+        for (int j = 0; j <= width; j++)
+        {
+
+            float soma = image[i][j].rgbtGreen + image[i][j].rgbtRed + image[i][j].rgbtBlue;
+            int average = (int)round(soma / 3);
+
+            image[i][j].rgbtGreen   = average;
+            image[i][j].rgbtBlue    = average;
+            image[i][j].rgbtRed     = average;
+
+        }
+    }
     return;
 }
 
