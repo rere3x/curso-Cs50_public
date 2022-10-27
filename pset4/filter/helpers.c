07c747aa5d1f13d0c8dbe6d4971c4e6461e533f3
rere3x <104224827+rere3x@users.noreply.github.com>
2022-10-27 23:29:11 +0000

    Thu, Oct 27, 2022, 11:29 PM +00:00

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -69,14 +69,12 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     int hafWidth = 0;
 
-    if (width < 2)
+    if (width <= 2)
         hafWidth = 0;
     else if(width % 2 == 0)
         hafWidth = (width / 2) - 1;
     else
         hafWidth = (width / 2) - 0.5;
-
-
 
      for (int i = 0; i <= height; i++)
     {
@@ -87,12 +85,49 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
             image[i][(width - 1) - j] = temp;
         }
     }
-
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    // copy the data to a temp image
+    RGBTRIPLE tempImage[height][width];
+      for (int i = 0; i <= height; i++)
+    {
+        for (int j = 0; j <= width; j++)
+        {
+            tempImage [i][j] = image[i][j];
+        }
+    }
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int count = 0;
+            float somaRed = 0;
+            float somaGreen = 0;
+            float somaBlue = 0;
+
+            for (int l = i-1; l <= i+1; l++)
+            {
+                for (int t = j-1; t <= j+1; t++)
+                {
+                    if((l >= 0 && t >=0) && (l <= height-1 && t <= width-1))
+                    {
+                    count++;
+
+                    somaRed += tempImage [l][t].rgbtRed;
+                    somaGreen += tempImage [l][t].rgbtGreen;
+                    somaBlue += tempImage [l][t].rgbtBlue;
+                    }
+                }
+            }
+            image[i][j].rgbtRed = (int)round(somaRed / count);
+            image[i][j].rgbtGreen = (int)round(somaGreen / count);
+            image[i][j].rgbtBlue = (int)round(somaBlue / count);
+            }
+        }
+
     return;
 }
