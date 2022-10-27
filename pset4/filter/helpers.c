ff15b4f43e1903e1ac4366c4c0f5b1297529f068
rere3x <104224827+rere3x@users.noreply.github.com>
2022-10-27 21:59:11 +0000

    Thu, Oct 27, 2022, 9:59 PM +00:00

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -23,12 +23,20 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+     RGBTRIPLE tempImage[height][width];
+      for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j <= width; j++)
+        {
+            tempImage [i][j] = image[i][j];
+        }
+    }
     for (int i = 0; i <= height; i++)
     {
         for (int j = 0; j <= width; j++)
         {
-
             
+
 
         }
     }
