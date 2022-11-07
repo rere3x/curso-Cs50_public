19d67571b053d9a428d75412e3c624dbcd07b67e
rere3x <104224827+rere3x@users.noreply.github.com>
2022-11-07 00:26:52 +0000

    Mon, Nov 7, 2022, 12:26 AM +00:00

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -12,10 +12,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             float soma = image[i][j].rgbtGreen + image[i][j].rgbtRed + image[i][j].rgbtBlue;
             int average = (int)round(soma / 3);
 
-            image[i][j].rgbtGreen   = average;
-            image[i][j].rgbtBlue    = average;
-            image[i][j].rgbtRed     = average;
-
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtRed = average;
         }
     }
     return;
@@ -38,27 +37,32 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
 
-            int sepiaRed =     round(.393 * tempImage[i][j].rgbtRed
-                                +  .769 * tempImage[i][j].rgbtGreen
-                                +  .189 * tempImage[i][j].rgbtBlue);
+            int sepiaRed = round     (.393 * tempImage[i][j].rgbtRed
+                                    + .769 * tempImage[i][j].rgbtGreen
+                                    + .189 * tempImage[i][j].rgbtBlue);
 
-            int sepiaGreen  =  round(.349 * tempImage[i][j].rgbtRed
-                            +  .686 * tempImage[i][j].rgbtGreen
-                            +  .168 * tempImage[i][j].rgbtBlue);
+            int sepiaGreen = round   (.349 * tempImage[i][j].rgbtRed
+                                    + .686 * tempImage[i][j].rgbtGreen
+                                    + .168 * tempImage[i][j].rgbtBlue);
 
-            int sepiaBlue   =  round(.272 * tempImage[i][j].rgbtRed
-                            +  .534 * tempImage[i][j].rgbtGreen
-                            +  .131 * tempImage[i][j].rgbtBlue);
+            int sepiaBlue = round    (.272 * tempImage[i][j].rgbtRed
+                                    + .534 * tempImage[i][j].rgbtGreen
+                                    + .131 * tempImage[i][j].rgbtBlue);
 
+            if (sepiaRed > 255)
+                image[i][j].rgbtRed = 255;
+            else
+                image[i][j].rgbtRed = sepiaRed;
 
-            if(sepiaRed > 255) image[i][j].rgbtRed = 255;
-            else image[i][j].rgbtRed = sepiaRed;
+            if (sepiaGreen > 255)
+                image[i][j].rgbtGreen = 255;
+            else
+                image[i][j].rgbtGreen = sepiaGreen;
 
-            if(sepiaGreen > 255) image[i][j].rgbtGreen = 255;
-            else image[i][j].rgbtGreen = sepiaGreen;
-
-            if(sepiaBlue > 255) image[i][j].rgbtBlue = 255;
-            else image[i][j].rgbtBlue = sepiaBlue;
+            if (sepiaBlue > 255)
+                image[i][j].rgbtBlue = 255;
+            else
+                image[i][j].rgbtBlue = sepiaBlue;
         }
     }
     return;
@@ -71,13 +75,12 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 
     if (width < 2)
         hafWidth = 0;
-    else if(width % 2 == 0)
+    else if (width % 2 == 0)
         hafWidth = (width / 2) - 1;
     else
         hafWidth = (width / 2) - 0.5;
 
-
-     for (int i = 0; i <= height; i++)
+    for (int i = 0; i <= height; i++)
     {
         for (int j = 0; j <= hafWidth; j++)
         {
@@ -94,11 +97,11 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     // copy the data to a temp image
     RGBTRIPLE tempImage[height][width];
-      for (int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
         for (int j = 0; j <= width; j++)
         {
-            tempImage [i][j] = image[i][j];
+            tempImage[i][j] = image[i][j];
         }
     }
     for (int i = 0; i < height; i++)
@@ -109,24 +112,24 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             int somaRed = 0;
             int somaGreen = 0;
             int somaBlue = 0;
-            
-            for (int l = i-1; l <= i+1; l++)
+
+            for (int l = i - 1; l <= i + 1; l++)
             {
-                for (int t = j-1; t <= j+1; t++)
+                for (int t = j - 1; t <= j + 1; t++)
                 {
-                    if((l >= 0 && t >=0) && (l <= height-1 && t <= width-1))
+                    if ((l >= 0 && t >= 0) && (l <= height - 1 && t <= width - 1))
                     {
-                    count++;
-                    somaRed += tempImage [l][t].rgbtRed;
-                    somaGreen += tempImage [l][t].rgbtGreen;
-                    somaBlue += tempImage [l][t].rgbtBlue;
+                        count++;
+                        somaRed += tempImage[l][t].rgbtRed;
+                        somaGreen += tempImage[l][t].rgbtGreen;
+                        somaBlue += tempImage[l][t].rgbtBlue;
                     }
                 }
             }
             image[i][j].rgbtRed = round(somaRed / count);
             image[i][j].rgbtGreen = round(somaGreen / count);
             image[i][j].rgbtBlue = round(somaBlue / count);
-            }
         }
+    }
     return;
 }
