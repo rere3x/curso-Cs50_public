e1827bd5b248c28deb0cc6ec3848f58ae8978e15
rere3x <104224827+rere3x@users.noreply.github.com>
2022-11-23 22:19:05 +0000

    Wed, Nov 23, 2022, 10:19 PM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
     fread(buffer, 4, 512, *f)
     if( buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xe0) == 0xe0)
 
-    sprintf(filen);
+    sprintf(filename, "%03i.jpg", 2);
 
 
 
