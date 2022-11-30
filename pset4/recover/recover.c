087cbec382d555add1f873b086a09ef8e83de57f
rere3x <104224827+rere3x@users.noreply.github.com>
2022-11-30 18:58:46 +0000

    Wed, Nov 30, 2022, 6:58 PM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -6,21 +6,22 @@ int main(int argc, char *argv[])
     //check if de file exist
     if(argv[1] == NULL)
     {
-    printf("Coloque o nome do arquivo a ser recuperado Ex. /recover card.raw")
+    printf("Coloque o nome do arquivo a ser recuperado Ex. /recover card.raw");
     return 1;
     }
     //open the file
-    FILE *f = fopen(agrv[1], "r");
+    FILE *f = fopen(argv[1], "r");
 
 
     int buffer[511];
 
     fread(buffer, 4, 512, *f)
+    
     if( buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xe0) == 0xe0)
 
     sprintf(filename, "%03i.jpg", 2);
     FILE *image = fopen(filename, "w");
-    
+
 
 
 
