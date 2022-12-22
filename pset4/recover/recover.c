a66c9a2e828bf1b05e131343712ff73598286d6a
rere3x <104224827+rere3x@users.noreply.github.com>
2022-12-22 20:35:36 +0000

    Thu, Dec 22, 2022, 8:35 PM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 
     int buffer[511];
 
-    fread(&BITMAPFILEHEADER, sizeof(BITMAPFILEHEADER), 1, *f)
+    fread(&BITMAPFILEHEADER, sizeof(BITMAPFILEHEADER), 512, *f)
 
     if( buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xe0) == 0xe0)
 
