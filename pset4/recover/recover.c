5f25a4e4c6512434d6bab811491adea7d8ca36bf
rere3x <104224827+rere3x@users.noreply.github.com>
2023-06-27 04:45:52 +0000

    Tue, Jun 27, 2023, 4:45 AM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 
     if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0 )
     {
-        
+        sprintf(filename, )
     }
 
 
