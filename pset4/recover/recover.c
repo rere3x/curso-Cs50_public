320cd498c860103b39efe5b26bc8d8ee668808da
rere3x <104224827+rere3x@users.noreply.github.com>
2023-06-27 05:34:21 +0000

    Tue, Jun 27, 2023, 5:34 AM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -30,9 +30,9 @@ int main(int argc, char *argv[])
     printf("Erro ao ler a memoria do arquivo.\n");
     }
     return 2;
-    if (i != 0)
     if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0 )
     {
+        if (i != 0)
 
         sprintf(image, "%03i.jpg", i);
         FILE *img = fopen(image, w);
