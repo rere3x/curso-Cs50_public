8e55240775c1ee09ed9126e59dbd5adeb1035c7f
rere3x <104224827+rere3x@users.noreply.github.com>
2023-06-27 06:25:22 +0000

    Tue, Jun 27, 2023, 6:25 AM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
             {
                 fclose(img);
             }
-            char
+            char image[10];
             sprintf(image, "%03i.jpg", i);
             FILE *img = fopen(image, "w");
             BYTE sucessul_write = fwrite (buffer, sizeof(BYTE), 512, img);
