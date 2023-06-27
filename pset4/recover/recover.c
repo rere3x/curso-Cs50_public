926f38b0fe6c9ac4df5c9052eb5250c16088e18b
rere3x <104224827+rere3x@users.noreply.github.com>
2023-06-27 04:44:38 +0000

    Tue, Jun 27, 2023, 4:44 AM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -27,6 +27,11 @@ int main(int argc, char *argv[])
     return 2;
     }
 
+    if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0 )
+    {
+        
+    }
+
 
     // separar em blocos do tamanho FAT(512) usando fread
 
