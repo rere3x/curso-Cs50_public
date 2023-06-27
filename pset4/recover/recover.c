1228fb87ed0cacbb33c36047d7936e45f19958cf
rere3x <104224827+rere3x@users.noreply.github.com>
2023-06-27 05:25:20 +0000

    Tue, Jun 27, 2023, 5:25 AM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -31,8 +31,11 @@ int main(int argc, char *argv[])
     }
     return 2;
 
-    if(i != 0, buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0 )
+    if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0 )
     {
+        if(i != 0)
+        fclose(img);
+        
         sprintf(image, "%03i.jpg", i);
         FILE *img = fopen(image, w);
         fwrite (buffer, BYTE, 512, *img);
