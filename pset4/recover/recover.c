e6943bcc6c577c01380f2624a369edef004044f1
rere3x <104224827+rere3x@users.noreply.github.com>
2023-08-04 19:17:07 +0000

    Fri, Aug 4, 2023, 7:17 PM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,25 +1,22 @@
-//check
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-
 
 typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
-    //check if the file exist and return a mesage if not
-    if(argv[1] == NULL)
+    // Verifica se o nome do arquivo foi passado como argumento
+    if (argv[1] == NULL)
     {
-    printf("Coloque o nome do arquivo a ser recuperado Ex. /recover card.raw \n");
-    return 1;
+        printf("Coloque o nome do arquivo a ser recuperado. Ex.: ./recover card.raw\n");
+        return 1;
     }
 
     const int BUFFER_SIZE = 512;
     BYTE buffer[BUFFER_SIZE];
 
-    //open the file
-    printf("Nome do arquivo: %s\n", argv[1]);
+    // Abre o arquivo "f"
     FILE *f = fopen(argv[1], "r");
     if (f == NULL)
     {
@@ -33,61 +30,53 @@ int main(int argc, char *argv[])
     int offsetcount = 1;
     size_t sucessul_read = 0;
 
+    do
+    {
+        // Le o bloco de dados do arquivo "f"
+        sucessul_read = fread(buffer, sizeof(BYTE), BUFFER_SIZE, f);
 
-
-
-    do{
-        sucessul_read = fread(buffer, sizeof(BYTE), BUFFER_SIZE, f);//le o bloco de dados do arquivo "f"
         if (sucessul_read == 0) // Se não houver mais dados para ler
         {
             break; // Sai do loop
         }
 
-
-        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0 )
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            if(img != NULL){
-            fclose(img);
+            if (img != NULL)
+            {
+                fclose(img);
+                img = NULL; // Define img como NULL para indicar que não há arquivo aberto
             }
 
             char image[10];
             sprintf(image, "%03d.jpg", count);
             img = fopen(image, "w");
-            size_t sucessul_write = fwrite (buffer, sizeof(BYTE), BUFFER_SIZE, img);
+            size_t sucessul_write = fwrite(buffer, sizeof(BYTE), BUFFER_SIZE, img);
 
             offsetcount++;
             count++;
-
-        }else if (img != NULL)
+        }
+        else if (img != NULL)
         {
-            size_t sucessul_write = fwrite (buffer, sizeof(BYTE), BUFFER_SIZE, img);
+            size_t sucessul_write = fwrite(buffer, sizeof(BYTE), BUFFER_SIZE, img);
 
             if (sucessul_write != BUFFER_SIZE)
-                {
-                    if (feof(img))
-                    {
-                        printf("Alcançou o fim do arquivo. \n");
-                        break;
-                        return 1;
-                    } else if (ferror(img))
-                    {
-                        perror("Erro ao ler o arquivo.");
-                        break;
-                        return 1;
-                    }
-
-                }
+            {
+                perror("Erro ao escrever no arquivo.");
+                fclose(img);
+                fclose(f);
+                return 1;
+            }
             fseek(img, (BUFFER_SIZE * offsetcount), SEEK_SET);
             offsetcount++;
         }
-        fseek(f, (BUFFER_SIZE * offsetcount), SEEK_SET);
-
-        }while (sucessul_read == BUFFER_SIZE);
-
-
-
+    } while (sucessul_read == BUFFER_SIZE);
 
     fclose(f);
+    if (img != NULL)
+    {
+        fclose(img);
+    }
 
     return 0;
 }
