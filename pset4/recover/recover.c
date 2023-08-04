ffaa7ff5b57c56229c3720be4b92f714bf1532a1
rere3x <104224827+rere3x@users.noreply.github.com>
2023-08-04 21:48:04 +0000

    Fri, Aug 4, 2023, 9:48 PM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -26,20 +26,23 @@ int main(int argc, char *argv[])
         printf("Erro ao abrir o arquivo. \n");
         return 1;
     }
+
     // Loop para ler e recuperar blocos de dados do arquivo "f"
     do
     {
         // Lê o bloco de dados do arquivo "f" e armazena em "buffer"
         sucessul_read = fread(buffer, sizeof(BYTE), BUFFER_SIZE, f);
 
-        if (sucessul_read == 0) // Se não houver mais dados para ler, sai do loop
+        // Se não houver mais dados para ler, sai do loop
+        if (sucessul_read == 0)
         {
             break;
         }
+
         // Verifica se o bloco de dados no "buffer" representa o início de um novo arquivo JPEG
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            //Confere se é o primeiro arquivo e se não for ele é fechado
+            // Confere se é o primeiro arquivo e se não for ele é fechado
             if (img != NULL)
             {
                 fclose(img);
@@ -49,16 +52,17 @@ int main(int argc, char *argv[])
             char image[10]; // Cria um nome para o novo arquivo JPEG baseado no valor atual de "count"
             sprintf(image, "%03d.jpg", count);
 
-            img = fopen(image, "w");// Abre um novo arquivo JPEG com o nome "image" no modo de escrita ("w")
-            size_t sucessul_write = fwrite(buffer, sizeof(BYTE), BUFFER_SIZE, img);// Escreve o bloco de dados no novo arquivo JPEG
+            img = fopen(image, "w"); // Abre um novo arquivo JPEG com o nome "image" no modo de escrita ("w")
+            size_t sucessul_write = fwrite(buffer, sizeof(BYTE), BUFFER_SIZE, img); // Escreve o bloco de dados no novo arquivo JPEG
 
-            count++;// Incrementa o valor de "count" para representar um novo arquivo criado
+            count++; // Incrementa o valor de "count" para representar um novo arquivo criado
         }
         else if (img != NULL)
         {
-            fseek(img, 0, SEEK_END);// Move o ponteiro do arquivo "img" para o final do arquivo existente
+            fseek(img, 0, SEEK_END); // Move o ponteiro do arquivo "img" para o final do arquivo existente
 
-            size_t sucessul_write = fwrite(buffer, sizeof(BYTE), BUFFER_SIZE, img);// Escreve o bloco de dados no arquivo JPEG existente
+            size_t sucessul_write = fwrite(buffer, sizeof(BYTE), BUFFER_SIZE, img); // Escreve o bloco de dados no arquivo JPEG existente
+
             // Verifica se todos os bytes foram gravados corretamente
             if (sucessul_write != BUFFER_SIZE)
             {
@@ -71,6 +75,7 @@ int main(int argc, char *argv[])
     }
     // Continua o loop enquanto houver dados a serem lidos do arquivo "f"
     while (sucessul_read == BUFFER_SIZE);
+
     // Fecha o arquivo "img" e "f"
     fclose(img);
     fclose(f);
