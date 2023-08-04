43c893f199387f638458f510896e8fec0463c5c2
rere3x <104224827+rere3x@users.noreply.github.com>
2023-08-04 22:53:12 +0000

    Fri, Aug 4, 2023, 10:53 PM +00:00

--- pset4/lab4/volume.c
+++ pset4/lab4/volume.c
@@ -23,14 +23,14 @@ int main(int argc, char *argv[])
     }
 
     // Open files and determine scaling factor
-    FILE *input = fopen(argv[1], "r");
+    input = fopen(argv[1], "r");
     if (input == NULL)
     {
         printf("Could not open file.\n");
         return 1;
     }
 
-    FILE *output = fopen(argv[2], "w");
+    output = fopen(argv[2], "w");
     if (output == NULL)
     {
         printf("Could not open file.\n");
