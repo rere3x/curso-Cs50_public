47432bcc3fb50be01414611f118122efeed4e14b
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-08 19:33:28 +0000

    Fri, Dec 8, 2023, 7:33 PM +00:00

--- cash.py
+++ cash.py
@@ -2,13 +2,22 @@ from cs50 import get_float
 
 def main ():
 
+    prodecutPrice = get_float("Your cash: ")
+    while prodecutPrice < 0.00001:
+        prodecutPrice = get_float("Your cash: ")
+
     answer = get_float("Your cash: ")
     while answer < 0.00001:
         answer = get_float("Your cash: ")
 
-    change = coins(answer)
+    change value
+
+    print(coins())
+
+
 
     def coins(value):
+
         coins = 0
 
         newValue = int(value * 100)
@@ -23,9 +32,9 @@ def main ():
             newValue = newValue%5
         if (newValue/1) > 0:
             coins = coins + (newValue/1)
+        return coins
 
         print("Coins owed: "+coins)
 
-        return coins
 
 main ()
