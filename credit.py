3e5725a372b3c36b4523178d9a9ecf0626037262
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-08 20:04:19 +0000

    Fri, Dec 8, 2023, 8:04 PM +00:00

--- credit.py
+++ credit.py
@@ -2,5 +2,5 @@
 from cs50 import get_int
 
 number = get_int("Card number: ")
-    while number.len != 15:
-        number= get_int("Card number: ")
+while number.len != 15:
+    number= get_int("Card number: ")
