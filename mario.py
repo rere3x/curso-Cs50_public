ff6b9cc8d2fc2665712040c89afa80f22e610d1c
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-08 18:11:59 +0000

    Fri, Dec 8, 2023, 6:11 PM +00:00

--- mario.py
+++ mario.py
@@ -3,7 +3,8 @@ from cs50 import get_int
 altura = get_int("indique a altura: ")
 bloco = 1
 for i in range(altura + 1):
-    altura -= 1
     print ("_" * altura, end="")
     print ("#" * bloco, end="")
+    print ()
+    altura -= 1
     bloco += 1
