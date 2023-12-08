bcb3da10c8381f2ec7ca9e5562c56b5fb9660910
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-08 20:42:29 +0000

    Fri, Dec 8, 2023, 8:42 PM +00:00

--- credit.py
+++ credit.py
@@ -1,9 +1,11 @@
 
 from cs50 import get_string
+from sys import exit
 
 number = get_string("Card number: ")
 while len(number) < 15:
     print("INVALID")
+    exit()
 
 if number[0] == '3':
     print("AMEX")
