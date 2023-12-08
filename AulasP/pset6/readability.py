30857bb905275781ba4d448d387a840fe4237cb6
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-08 21:17:04 +0000

    Fri, Dec 8, 2023, 9:17 PM +00:00

--- AulasP/pset6/readability.py
+++ AulasP/pset6/readability.py
@@ -11,8 +11,8 @@ y = " "
 for y in text:
     words += 1
 
-letters = len(text) - (senteses + words)
+letters = len(text) - (sentenses + words)
 
 letters = letters/words
 
-print((0.0588 * letters) - (0.296 * senteses) - 15.8)
+print((0.0588 * letters) - (0.296 * sentenses) - 15.8)
