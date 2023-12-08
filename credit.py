9ba874160b5a3a5241d6e050260944a8b0e62e00
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-08 20:34:52 +0000

    Fri, Dec 8, 2023, 8:34 PM +00:00

--- credit.py
+++ credit.py
@@ -2,7 +2,7 @@
 from cs50 import get_string
 
 number = get_string("Card number: ")
-while len(number) != 15:
+while len(number) < 15:
     number= get_string("Card number: ")
 
 if number[0] == '3':
