90899f82fb15affdeb968a4938bc30b0f3faa68f
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-08 21:05:52 +0000

    Fri, Dec 8, 2023, 9:05 PM +00:00

--- AulasP/pset6/readability.py
+++ AulasP/pset6/readability.py
@@ -2,6 +2,11 @@ from cs50 import get_string
 
 text = get_string("Digite seu texto: ")
 
-sentenses   = for 
-words       =
-letters     =
+for ['?', '!', '.'] in text:
+    sentenses += 1
+for ' ' in text
+    words += 1
+
+letters = len(text) - (senteses + words)
+
+print (0,0588 * letters - 0,296 * senteses - 15,8)
