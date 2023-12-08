6c911f8d42c0d299cfcb8d2c89b843973fc6a98e
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-08 19:30:13 +0000

    Fri, Dec 8, 2023, 7:30 PM +00:00

--- cash.py
+++ cash.py
@@ -12,17 +12,17 @@ def main ():
         coins = 0
 
         newValue = int(value * 100)
-        if (x/25) > 0:
-            coins = coins + (x/25)
-            x = x%25
-        if (x/10) > 0:
-            coins = coins + (x/10)
-            x = x%10
-        if (x/5) > 0:
-            coins = coins + (x/5)
-            x = x%5
-        if (x/1) > 0:
-            coins = coins + (x/1)
+        if (newValue/25) > 0:
+            coins = coins + (newValue/25)
+            newValue = newValue%25
+        if (newValue/10) > 0:
+            coins = coins + (newValue/10)
+            newValue = newValue%10
+        if (newValue/5) > 0:
+            coins = coins + (newValue/5)
+            newValue = newValue%5
+        if (newValue/1) > 0:
+            coins = coins + (newValue/1)
 
         print("Coins owed: "+coins)
 
