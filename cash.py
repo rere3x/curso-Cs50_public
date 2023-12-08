2887b12feff6225364ee0f6ca16818c8c364f67a
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-08 19:54:56 +0000

    Fri, Dec 8, 2023, 7:54 PM +00:00

--- cash.py
+++ cash.py
@@ -8,36 +8,27 @@ def main():
 
     print(coins(answer))
 
-
-
 def coins(value):
 
     coins = 0
 
     newValue = int(value * 100)
-    print(newValue)
+
     if (newValue/25) > 0:
         coins = coins + int(newValue/25)
         newValue = newValue%25
-        print(f"new {newValue}")
-        print (f"coins {coins}")
 
     if (newValue/10) > 0:
         coins = coins + int(newValue/10)
         newValue = newValue%10
-        print(f"new {newValue}")
-        print (f"coins {coins}")
 
     if (newValue/5) > 0:
         coins = coins + int(newValue/5)
         newValue = newValue%5
-        print(f"new {newValue}")
-        print (f"coins {coins}")
 
     if (newValue/1) > 0:
         coins = coins + int(newValue/1)
-        print(f"new {newValue}")
-        print (f"coins {coins}")
+
     return coins
 
 main()
