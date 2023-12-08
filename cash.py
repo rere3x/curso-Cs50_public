bfdd269e5489aa87dad27fd63ae5f00ad40fc4ad
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-08 19:38:43 +0000

    Fri, Dec 8, 2023, 7:38 PM +00:00

--- cash.py
+++ cash.py
@@ -3,8 +3,8 @@ from cs50 import get_float
 
 
 answer = get_float("Your cash: ")
-    while answer < 0.00001:
-        answer = get_float("Your cash: ")
+    while answer < 0.01:
+    answer = get_float("Your cash: ")
 
 print(coins(answer))
 
