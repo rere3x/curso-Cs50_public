370b0b275ac5d435f1069b1cfc415abecb4d7cbc
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-08 20:25:58 +0000

    Fri, Dec 8, 2023, 8:25 PM +00:00

--- credit.py
+++ credit.py
@@ -1,8 +1,11 @@
 
 from cs50 import get_string
 
-number = get_string("Card number: ")
+number = get_int("Card number: ")
 while len(number) != 15:
     number= get_string("Card number: ")
+
+if number[0] == 3:
+    print("MASTERCAD")
 
 
