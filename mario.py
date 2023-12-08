870972089989e175b3f39b40cf33d01dc7e14e81
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-08 17:49:19 +0000

    Fri, Dec 8, 2023, 5:49 PM +00:00

--- mario.py
+++ mario.py
@@ -1,8 +1,9 @@
 from cs50 import get_int
 
 altura = get_int("indique a altura: ")
-
+i = 1
 while True:
-    print(" "*(altura - 1))
-    print("#" * altura + 1)
+    print (" " * (altura - 1))
+    print ("#" * i)
     altura -= 1
+    i      += 1
