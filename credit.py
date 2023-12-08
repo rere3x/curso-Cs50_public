4492e3728b997325f6a3011cf10633244a3f52ba
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-08 20:30:37 +0000

    Fri, Dec 8, 2023, 8:30 PM +00:00

--- credit.py
+++ credit.py
@@ -1,11 +1,17 @@
 
-from cs50 import get_int
+from cs50 import get_string
 
-number = get_int("Card number: ")
+number = get_string("Card number: ")
 while len(number) != 15:
-    number= get_int("Card number: ")
+    number= get_string("Card number: ")
 
-if number[0] == 3:
+if number[0] == '3':
+    print("AMERICAN EXPRESS")
+elif number[0] == '5':
     print("MASTERCAD")
+elif number[0] == '4':
+    print("VISA")
+else
+    print("INVALID")
 
 
