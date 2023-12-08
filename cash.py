a3500c1b0b31c750b65bde67f7745ca95d9c614e
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-08 19:37:49 +0000

    Fri, Dec 8, 2023, 7:37 PM +00:00

--- cash.py
+++ cash.py
@@ -1,34 +1,35 @@
 from cs50 import get_float
 
-def main ():
 
-    answer = get_float("Your cash: ")
+
+answer = get_float("Your cash: ")
     while answer < 0.00001:
         answer = get_float("Your cash: ")
 
-    print(coins(answer))
+print(coins(answer))
 
 
 
-    def coins(value):
+def coins(value):
 
-        coins = 0
+    coins = 0
 
-        newValue = int(value * 100)
-        if (newValue/25) > 0:
-            coins = coins + (newValue/25)
-            newValue = newValue%25
-        if (newValue/10) > 0:
-            coins = coins + (newValue/10)
-            newValue = newValue%10
-        if (newValue/5) > 0:
-            coins = coins + (newValue/5)
-            newValue = newValue%5
-        if (newValue/1) > 0:
-            coins = coins + (newValue/1)
-        return coins
+    newValue = int(value * 100)
+    if (newValue/25) > 0:
+        coins = coins + (newValue/25)
+        newValue = newValue%25
 
-        print("Coins owed: "+coins)
+    if (newValue/10) > 0:
+        coins = coins + (newValue/10)
+        newValue = newValue%10
+
+    if (newValue/5) > 0:
+        coins = coins + (newValue/5)
+        newValue = newValue%5
+
+    if (newValue/1) > 0:
+       coins = coins + (newValue/1)
+    return coins
 
 
-main ()
+
