4218a3c3f8ce165002315042fdf90885107dd2fb
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-08 20:38:57 +0000

    Fri, Dec 8, 2023, 8:38 PM +00:00

--- credit.py
+++ credit.py
@@ -4,6 +4,7 @@ from cs50 import get_string
 number = get_string("Card number: ")
 while len(number) < 15:
     print("INVALID")
+    
     number= get_string("Card number: ")
 
 if number[0] == '3':
