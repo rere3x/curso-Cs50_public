3d85bfc2dd5d4daaaee24c1bcdd1d61e3992f3fb
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-09 01:08:39 +0000

    Sat, Dec 9, 2023, 1:08 AM +00:00

--- AulasP/pset6/readability.py
+++ AulasP/pset6/readability.py
@@ -1,28 +1,22 @@
 from cs50 import get_string
+import string
 
+text        = get_string("Digite seu texto: ")
+letters    = 0
+words       = 0
+sentenses   = 0
 
-text = get_string("Digite seu texto: ")
-words =
-sentenses = 0
 for i in range(len(text)):
     if str.isalpha(text[i]):
-        words +=1
+        letters +=1
 for i in range(len(text)):
     if str.isspace(text[i]):
         words +=1
 for i in range(len(text)):
     if str.find(string.punctuation) in text:
-        
+        sentenses +=1
 
+letters = letters/words
+print(letters)
 
-
-
-
-
-
-
-
-#letters = letters/words
-#print(letters)
-
-#print((0.0588 * letters) - (0.296 * sentenses) - 15.8)
+print((0.0588 * letters) - (0.296 * sentenses) - 15.8)
