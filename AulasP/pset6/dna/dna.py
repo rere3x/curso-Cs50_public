f3136ec1dea642573dcf18ed68744178d49fceab
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-12 17:27:03 +0000

    Tue, Dec 12, 2023, 5:27 PM +00:00

--- AulasP/pset6/dna/dna.py
+++ AulasP/pset6/dna/dna.py
@@ -2,10 +2,14 @@ import csv
 import sys
 
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
     print("Usage: python dna.py data.csv sequence.txt")
     sys.exit(1)
 
 with open(sys.argv[1], 'r') as database:
     database_reader = csv.reader(database)
-    
+    database_data = [row for row in database_reader]
+
+with open(sys.argv[2], 'r') as sequence:
+    sequence_reader = csv.reader(sequence)
+   
