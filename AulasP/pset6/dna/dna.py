ab38c8962439b5d43120e28749ddd65d8d0b2d6c
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-12 18:50:43 +0000

    Tue, Dec 12, 2023, 6:50 PM +00:00

--- AulasP/pset6/dna/dna.py
+++ AulasP/pset6/dna/dna.py
@@ -15,6 +15,6 @@ with open(sys.argv[2], 'r') as sequence_file:
 
 for i in range(len(sequence)):
     for j in range(len(database[0]) - 1)
-        for l in range(len(database[0][j + 1]))
+        for l in range(len(database[0][j]))
 
 
