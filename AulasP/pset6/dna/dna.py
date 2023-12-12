942e1ab50bb5beab9c12ad1250d68e45c0d904c1
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-12 19:04:24 +0000

    Tue, Dec 12, 2023, 7:04 PM +00:00

--- AulasP/pset6/dna/dna.py
+++ AulasP/pset6/dna/dna.py
@@ -15,9 +15,9 @@ with open(sys.argv[2], 'r') as sequence_file:
 
 for i in range(len(sequence)):
     match_found = False
-    for j in range(len(database[0]) - 1):
-        for l in range(len(database[0][j + 1])):
-            if database[0][j + 1][l] != sequence[i]:
+    for j in range(len(database_data[0]) - 1):
+        for l in range(len(database_data[0][j + 1])):
+            if database_data[0][j + 1][l] != sequence[i]:
                 match_found = True
                 break
             if match_found:
