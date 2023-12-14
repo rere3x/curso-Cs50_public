2c214e07df57f8b72a0427189b8e5b9ce0bc1642
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-14 21:14:05 +0000

    Thu, Dec 14, 2023, 9:14 PM +00:00

--- AulasP/pset6/dna/dna.py
+++ AulasP/pset6/dna/dna.py
@@ -20,7 +20,7 @@ for i in range(len(sequence)):
     for j in range(len(database_data[0]) - 1):
         test_count = 0
 
-        for l in range(len(database_data[j][l])):
+        for l in range(len(database_data[0][l])):
             if (l + i) >= len(sequence):
                 break
             elif database_data[j + 1][l + 1] != sequence[l + i]:
