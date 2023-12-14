e8337e68a427b81b3999833767aeb1960b1a5840
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-14 23:05:35 +0000

    Thu, Dec 14, 2023, 11:05 PM +00:00

--- AulasP/pset6/dna/dna.py
+++ AulasP/pset6/dna/dna.py
@@ -3,18 +3,18 @@ from sys import argv, exit
 
 
 
-def main()
-    if argv != 3:
-        print(f" Usage: python {argv[0]} data.csv sequence.txt")
+def main():
+    if len(argv) != 3:
+        print(f"Usage: python {argv[0]} data.csv sequence.txt")
         exit(1)
 
-        _,data_file, sequence_file = agrv
+    _, data_file, sequence_file = argv
 
-        content = read_csv(data)
-        seq_count = read_dna_sequence(sequence_file, content["columns"][1:])
+    content = read_csv(data_file)
+    seq_count = read_dna_sequence(sequence_file, content["columns"][1:])
 
-        print(identify_dna(content["data"], seq_count))
-        exit(0)
+    print(identify_dna(content["data"], seq_count))
+    exit(0)
 
 
 def read_csv(path):
@@ -22,17 +22,22 @@ def read_csv(path):
         reader = csv.DictReader(csvfile)
         return {"columns": reader.fieldnames, "data": list(reader)}
 
-def Max_consecutive_reps(substring,strig)
+def Max_consecutive_reps(substring,strig):
     total_reps = len(string)
     while (substring * total_reps) not in string and total_reps > 0:
         total_reps -= 1
     return total_reps
 
 
-def read_dna_sequence(path, sequences)
+def read_dna_sequence(path, sequences):
     with open(path) as file:
         dna_seq = file.read()
         return {seq: str(max_consecutive_reps(seq, dna_seq)) for seq in sequences}
+def indentify_dna(user_data, seq_count):
+    return next(
+        (data["name"] for data in user_data if seq_count.items() <= data.items()),
+        "No match",
+    )
 main()
 
 
