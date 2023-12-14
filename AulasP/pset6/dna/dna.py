119d942815f6b28bb31abcde5e7c1c50cf171b9a
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-14 19:38:06 +0000

    Thu, Dec 14, 2023, 7:38 PM +00:00

--- AulasP/pset6/dna/dna.py
+++ AulasP/pset6/dna/dna.py
@@ -38,9 +38,10 @@ print(dna_count[2])
 print()
 for i in range(len(database_data) - 1):
     right_count = 0
+    print("test")
     for l in range(len(database_data[i + 1]) - 1):
         print(l)
-        #print(database_data[i + 1][l + 1])
+
         if dna_count[i] != int(database_data[i + 1][l + 1]):
             break
         else:
