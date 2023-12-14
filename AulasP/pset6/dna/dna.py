b2e000bfa7bc52f239389ae2f7ff85fb7ff47181
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-14 22:30:09 +0000

    Thu, Dec 14, 2023, 10:30 PM +00:00

--- AulasP/pset6/dna/dna.py
+++ AulasP/pset6/dna/dna.py
@@ -3,8 +3,20 @@ from sys import argv, exit
 
 
 
+def main()
+    if argv != 3:
+        print(f" Usage: python {argv[0]} data.csv sequence.txt")
+        exit(0)
+
+        _,data_file, sequence_file = agrv
 
 
+
+def read_csv(path):
+    with open(path) as csvfile:
+        reader = csv.DictReader(csvfile)
+        return {"columns": reader.fieldnames, "data":}
+main()
 
 
 
