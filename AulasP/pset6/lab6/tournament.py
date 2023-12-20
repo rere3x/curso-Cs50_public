bbdc57f7561c00abc6b79af9d64c351b059b34b4
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-20 20:15:05 +0000

    Wed, Dec 20, 2023, 8:15 PM +00:00

--- AulasP/pset6/lab6/tournament.py
+++ AulasP/pset6/lab6/tournament.py
@@ -17,11 +17,11 @@ def main():
     _,teams_to_read = argv
 
     with open(teams_to_read) as teams_read
-    reader = csv.DictReader(teams_read)
-    
-
+        reader = csv.DictReader(teams_read)
+        
     teams = []
-
+    for row in reader
+        teams.append(row)
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
