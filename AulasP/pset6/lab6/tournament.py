ffc3666e79ef88fcf7469dd72a48b1ba0e23d65a
rere3x <104224827+rere3x@users.noreply.github.com>
2023-12-21 00:02:32 +0000

    Thu, Dec 21, 2023, 12:02 AM +00:00

--- AulasP/pset6/lab6/tournament.py
+++ AulasP/pset6/lab6/tournament.py
@@ -57,8 +57,7 @@ def simulate_round(teams):
 
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
-    winning_team
-
+    
 
 
 if __name__ == "__main__":
